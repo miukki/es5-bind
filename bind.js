6de747cb289224cf37763c74a79a5656da257e2f
Anja Ishmukhametova <miukki@gmail.com>
2015-06-24 15:19:24 +0800

    test tasks

--- bind.js
+++ bind.js
@@ -1,4 +1,4 @@
-//example from test task
+//create shim for bind
 
 if (!Function.prototype.bind) {
   Function.prototype.bind = function () {
@@ -9,3 +9,82 @@ if (!Function.prototype.bind) {
     }
   };
 }
+
+
+
+
+/*
+ * Create a function 'tick' which prints consecutive numbers,
+ * starting from 0.
+ * Do not use any other global names except 'tick'.
+ */
+ /*
+ * Example:
+ * tick(); // prints 0
+ * tick(); // prints 1
+ * tick(); // prints 2
+ */
+
+var tick = (function() {
+    var count= 0;
+    return function () {
+        return count++;
+    }
+})()
+
+---------------------------------------
+
+// Without running the following code, tell me what will be printed:
+
+(function(){
+    var x = "abc";
+    (function(){
+        var y = x;
+        var x = "123";
+        console.log(y); // ??? undefined
+    })();
+})();
+
+
+//!!!!if in code
+
+fn() //we will get error fn is not a function
+var fn = function() {}
+
+//!!!!but if
+
+fn() //everything will be alright
+function fn () {}
+
+/*
+the reason is called “hoisting” in javascript.
+It means that variables declarations go to the top of the scope.
+But variable assignments stay in the original line”.
+So this: var y = x; var x = “abc”, is equivalent to this: var y; var x; y = x; x = “abc”.
+and this is why we get y === undefined.
+*/
+
+
+----------------------------------------------
+
+// write a function 'sum' that takes an arbitrary number of arguments
+// and calculates the sum of them.
+// examples:
+// sum(1, -1) returns 0
+// sum (100, 200, 1) returns 301
+
+function sum() {
+    var sum = 0;
+    var arr =  Array.prototype.slice.apply(arguments);
+    for(var i=0; i< arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+//es-5
+[0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, index, array) {
+  return previousValue + currentValue;
+});
+
+
---
+++ test.js
@@ -0,0 +1,103 @@
+//unflatten array
+var people =  [
+    {
+        "id": "12",
+        "parentId": "0",
+    },
+    {
+        "id": "6",
+        "parentId": "7",
+    },
+    {
+        "id": "7",
+        "parentId": "12",
+    },
+    {
+        "id": "17",
+        "parentId": "12",
+    },
+    {
+        "id": "8",
+        "parentId": "6",
+    }
+
+]
+
+
+var unflatten = function(arr, parent, tree) {
+  tree = typeof tree !== 'undefined' ? tree : [];
+  parent = typeof parent !== 'undefined' ? parent : { id: 0 };
+
+  var childrens = Array.prototype.filter.call( arr, function(child){ return child.parentId == parent.id; });
+  parent['childrens'] = childrens;
+  return parent
+}
+
+console.log(people.map(function(item){return unflatten(people, {id: item.id}).childrens.length ? unflatten(people, {id: item.id}) : undefined }))
+
+
+//horizontal sort verian 1 with sort
+var data = {
+    text: "zero",
+    childrens: [
+        {
+            text: 'a',
+            childrens: [
+                {
+                    text: "c",
+                    childrens: [
+                        {
+                            text: 'd',
+                            childrens: [{ text: 'z'}]
+                        },
+                        {
+                            text: 'e',
+                            childrens: []
+                        },
+                        {
+                            text: 'f',
+                            childrens: []
+                        },
+                        {
+                            text: 'g',
+                            childrens: []
+                        },
+                        {
+                            text: 'h',
+                            childrens: []
+                        },
+                        {
+                            text: 'i',
+                            childrens: []
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            text: 'b',
+            childrens: []
+        }
+    ]
+};
+
+
+//horizontal sort
+var fn = function(data, tree, level) {
+  level = typeof level !== 'undefined' ? level : 0;
+  tree = typeof tree !== 'undefined' ? tree : [];
+
+  tree.push(data['text'] + ' level ' +  level);
+  tree.sort(function(a,b){
+    if(/level\s\d/.exec(a)[0]>/level\s\d/.exec(b)[0]) {
+        return 1;
+    }
+    return -1;
+  })
+
+  if (data.childrens ) {level++; data.childrens.forEach(function(el) {fn(el, tree, level)}); }
+  return tree;
+
+}
+
+//console.log('result: ' + fn(data));
